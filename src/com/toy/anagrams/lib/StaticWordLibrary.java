2be9c381a9d6616774dd38a944e8419914140a6d
S525884 <S525884@NW32711.nwmissouri.edu>
2016-09-08 11:07:14 -0500

    added bearcat word in StaticWordLibrary.java modified bearcat to bearcats

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -37,7 +37,7 @@ package com.toy.anagrams.lib;
 final class StaticWordLibrary extends WordLibrary {
 
     private static final String[] WORD_LIST = {
-        "bearcat",
+        "bearcats",
         "abstraction",
         "ambiguous",
         "arithmetic",
